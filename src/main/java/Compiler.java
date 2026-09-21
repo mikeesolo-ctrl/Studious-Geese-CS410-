08ec3348d6c456f5ef5444af3d9f112a68bb17ff
SpudSensei <spudsbuddy2202@gmail.com>
2026-09-21 13:25:36 -0400

    State Table

--- src/main/java/Compiler.java
+++ src/main/java/Compiler.java
@@ -1,8 +1,47 @@
-import java.io.*;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Compiler {
-    static void main() {
-        System.out.println("Hello World");
+
+    public static List<String> tokenize(String src) {
+        List<String> tokens = new ArrayList<>();
+        int pos = 0;
+
+        while (pos < src.length()) {
+            char c = src.charAt(pos);
+            if (Character.isWhitespace(c)) {
+                pos++;
+                continue;
+            }
+
+            int state = Tables.S_START;
+            int i = pos;
+            int lastAcceptState = Tables.ERR;
+            int lastAcceptEnd = pos;
+
+            while (i < src.length()) {
+                int next = Tables.TRANSITIONS[state][Tables.inputOf(src.charAt(i))];
+                if (next == Tables.ERR)
+                    break;
+                state = next;
+                i++;
+                if (Tables.ACCEPTING[state]) {
+                    lastAcceptState = state;
+                    lastAcceptEnd = i;
+                }
+            }
+
+            if (lastAcceptState == Tables.ERR) {
+                throw new RuntimeException("Unexpected token '" + c + "' at position " + pos);
+            }
+
+            String lexeme = src.substring(pos, lastAcceptEnd);
+            tokens.add(Tables.TOKEN_NAMES[lastAcceptState] + "(" + lexeme + ")");
+            pos = lastAcceptEnd;
+        }
+        return tokens;
+    }
+
+    public static void main(String[] args) {
     }
 }
