0c73a1533bf669017b21e6be3b22453b85ca4add
Michael Solomon <mikeesolo@gmail.com>
2026-09-24 14:17:21 -0400

    Merged Scanner Output int Michael Branch

--- src/main/java/Compiler.java
+++ src/main/java/Compiler.java
@@ -1,9 +1,5 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.File;
-import java.io.IOException;
 
 public class Compiler {
 
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -2,17 +2,12 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Scanner;
+import java.io.FileWriter;
 import java.util.List;
 
 public class main {
     public static void main(String[] args){
 
-        Scanner scnr = new Scanner(System.in); //Create Scanner to get file name
-        String fileName = scnr.next(); //Set the next System in to fileName
-
-        File file = new File(fileName); //Create file var with that name
-        String fileString; //Where the file as one string will be stored
         File file = new File("TestFile.txt"); //Create file var with that name
         String fileString = ""; //Where the file as one string will be stored
 
